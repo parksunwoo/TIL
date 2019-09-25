aba5bf8f95075bd00629de325d0f65779dc4f68f
sunu <psw261@gmail.com>
2019-09-26 00:00:23 +0900

    add deep learning from scratch2 code

---
+++ deep-learning/forward_net.py
@@ -0,0 +1,23 @@
+import nump as np
+
+class Sigmoid:
+    def __init__(self):
+        self.params = []
+
+    def __forward__(self, x):
+        return 1 / (1 + np.exp(-x))
+
+class Affine:
+    def __init__(self, W, b):
+        self.params = [W, b]
+
+    def __forward(self, x):
+        W, b = self.params
+        out = np.matmul(x, W) + b
+        return out
+
+class TwoLayerNet:
+    def __init__(self, input_size, hidden_size, output_size):
+        I, H, O = input_size, hidden_size, output_size
+
+        W1 = np.random.randn(I, H)
