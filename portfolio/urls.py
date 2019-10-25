3543f5525d302a633ff7abdb013ec10132c98b9e
SUNWOOPARK <psw261@gmail.com>
2019-10-26 07:46:56 +0900

    Add jobs and blog apps

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,4 +1,4 @@
-"""portfolio URL Configuration
+"""portfolio-project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
