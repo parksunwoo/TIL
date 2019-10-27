479fe28a3b2ddfe44cd015506b457795760970a3
SUNWOOPARK <psw261@gmail.com>
2019-10-28 06:15:07 +0900

    add blog model, bootstrap

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -17,7 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from jobs.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
