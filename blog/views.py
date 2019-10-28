3e43fd402b55f78ad8cd38370f1e1de02addf7d4
SUNWOOPARK <psw261@gmail.com>
2019-10-28 23:45:11 +0900

    added bootstrap and blogs showing up

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Blog
 # Create your views here.
+def allblogs(request):
+    blogs= Blog.objects
+    return render(request, 'blog/allblogs.html', {'blogs':blogs})
