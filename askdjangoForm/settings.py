631082891067b08b90467d32ed59116b0bd157f5
SUNWOOPARK <psw261@gmail.com>
2019-12-26 23:49:24 +0900

    add widget

--- askdjangoForm/settings.py
+++ askdjangoForm/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'myapp',
     'accounts',
+    'django_summernote',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
--- myapp/admin.py
+++ myapp/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = '__all__'
+
 admin.site.register(Comment)
