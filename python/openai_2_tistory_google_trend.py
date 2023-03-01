dca5674e46093d01ef884d7e93bf823e8a083fa9
parksunwoo <psw261@gmail.com>
2023-03-01 22:36:54 +0900

    split config & add gitignore

--- python/openai_2_tistory_google_trend.py
+++ python/openai_2_tistory_google_trend.py
@@ -10,33 +10,39 @@ from time import sleep
 from urllib.parse import urlparse
 from googletrans import Translator
 
+with open('conf.json') as f:
+    config = json.load(f)
+
 # Insert your OpenAI API key here
-openai.api_key = "sk-7xxxxxxxxx"
-CLIENT_ID = "xxx"
-CLIENT_SECRET = "xxx"
-REDIRECT_URI = "https://xxx.tistory.com"
-USERNAME = "xxx@kakao.com"
-PASSWORD = "xxx"
-BLOGNAME = "xxx"
+openai.api_key = config['openai.api_key']
+CLIENT_ID = config['CLIENT_ID']
+CLIENT_SECRET = config['CLIENT_SECRET']
+REDIRECT_URI = config['REDIRECT_URI']
+USERNAME = config['USERNAME']
+PASSWORD = config['PASSWORD']
+BLOGNAME = config['BLOGNAME']
 
 # TODO
 # https://trends.google.com/trends/yis/2022/US/
-def get_top_10_trending_keywords():
+def get_top_10_trending_keywords(pn):
     pytrends = TrendReq()
 
     # Get the top 10 trending keywords
-    trending_searches_df = pytrends.trending_searches(pn='united_states')
-    # trending_searches_df = pytrends.trending_searches(pn='south_korea')
-    top_10_trends = trending_searches_df.head(10).values.tolist()
+    trending_searches_df = pytrends.trending_searches(pn=pn)
+    top_10_trends = trending_searches_df.head(30).values.tolist()
 
     return top_10_trends
 
-def generate_summarize_text(keywords):
+def generate_summarize_text(keywords, pn):
+    if pn == 'south_korea':
+        prompt = "Please answer the topic following the conditions below Topic:" + "\n".join(keywords) +" Length: around 600 words. Format: markdown. Answer me in Korean Includes title Includes subtitles and detail descriptions. Voice and style guide: Write at a 5th grade level. Use clear and simple language, even when explaining complex topics. Bias toward short sentences. Avoid jargon and acronyms. Contend Goal: Blog."
+    else:
+        prompt="Summarize the contents of the main articles searched for by "+ "\n".join(keywords) +" and summarize them in an 300 words",
+
     completions = openai.Completion.create(
         engine="text-davinci-003",
-        prompt="Summarize the contents of the main articles searched for by "+ "\n".join(keywords) +" and summarize them in an 700 words",
-        # prompt="\n".join(keywords) +" 해당 키워드로 https://naver.com 에서 검색되는 주요 기사들의 내용을 종합해서 3000자 분량으로 요약해주세요",
-        max_tokens=3000,
+        prompt=prompt,
+        max_tokens=1200,
         n=1,
         stop=None,
         temperature=0.5,
@@ -73,7 +79,7 @@ def get_access_token():
     button = driver.find_element(By.CLASS_NAME, "submit")
     button.click()
 
-    sleep(3)
+    sleep(1)
 
     button = driver.find_element(By.CLASS_NAME, "confirm")
     button.click()
@@ -96,16 +102,24 @@ def get_access_token():
     access_token = response.text.split("=")[1]
     return access_token
 
-def wirte_tistroy_post(access_token, keywords, generated_summarize_text):
+def wirte_tistroy_post(access_token, keywords, content):
     # Replace these values with your own Tistory API credentials
     blog_name = BLOGNAME
 
     # The title and content of the article you want to post
     title = keywords + " 주요기사 요약"
-    content = generated_summarize_text
 
-    post_url = f"https://www.tistory.com/apis/post/write?access_token={access_token}&blogName={blog_name}&title={title}&content={content}&visibility=3&category=1083664&output=json"
-    response = requests.post(post_url)
+    post_url = "https://www.tistory.com/apis/post/write?"
+    params = {
+        'access_token': access_token,
+        'blogName': blog_name,
+        'title': title,
+        'content': content,
+        'visibility': 0,
+        'category': 0,
+        'output': "json"
+    }
+    response = requests.post(post_url, params=params)
 
     print('5'*100)
     if response.status_code == 200:
@@ -118,29 +132,49 @@ def wirte_tistroy_post(access_token, keywords, generated_summarize_text):
         return response.status_code
 
 print('0'*100)
-top_10_trends = get_top_10_trending_keywords()
 access_token = get_access_token()
+top_10_trends_us = get_top_10_trending_keywords(pn='united_states')
+print(top_10_trends_us)
 
-print(top_10_trends)
-print(access_token)
+top_10_trends_ko = get_top_10_trending_keywords(pn='south_korea')
+print(top_10_trends_ko)
 
-for keyword_list in top_10_trends:
+translator = Translator()
+
+for keyword_list in top_10_trends_us:
     print('1'*100)
     keyword = keyword_list[0]
-
+    # keyword = keyword_list
     print(keyword)
 
     print('2'*100)
-    generated_summarize_text = generate_summarize_text(keyword)
-
+    generated_summarize_text = generate_summarize_text(keyword, pn='united_states')
     print(generated_summarize_text)
     
-    translator = Translator()
-    translations = translator.translate(generated_summarize_text, src='en', dest='ko')
-
     print('3'*100)
+    translations = translator.translate(generated_summarize_text, src='en', dest='ko')
     print(translations.text + '\n\n' + '---' + '\n\n원문\n' + generated_summarize_text)
     content = translations.text + '\n\n' + '---' + '\n\n원문\n' + generated_summarize_text
+    print(len(content))
+
+    print('4'*100)
+    status_code = wirte_tistroy_post(access_token, keyword, content)
+
+for keyword_list in top_10_trends_ko:
+    print('1'*100)
+    keyword = keyword_list[0]
+    # keyword = keyword_list
+    print(keyword)
+
+    print('2'*100)
+    generated_summarize_text = generate_summarize_text(keyword, pn='south_korea')
+    print(generated_summarize_text)
+    
+    print('3'*100)
+    translations = translator.translate(keyword, src='en', dest='ko')    
+    keyword = translations.text
+    content = generated_summarize_text
+    print(len(content))
 
     print('4'*100)
     status_code = wirte_tistroy_post(access_token, keyword, content)
